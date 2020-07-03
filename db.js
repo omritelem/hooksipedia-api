e2dd64c01236fc7a14a215206260100540b52e95
omri telem <omritelemz@gmail.com>
2020-07-03 20:53:29 +0300

    fix: connection 2

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv/config');
 
 const connectDb = callback => {
     mongoose.connect(
-		// process.env.DATABASE_URL,
-		'mongodb+srv://omri_telem:35am7hwsoeS@cluster0-byakh.mongodb.net/Hooksipedia?retryWrites=true&w=majority',
+		process.env.DATABASE_URL,
         {
             useUnifiedTopology: true,
             useNewUrlParser: true,
