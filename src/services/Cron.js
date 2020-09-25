2b03914fbd1a2cee8537d63834dff6df2a65fa02
omri telem <omritelemz@gmail.com>
2020-09-25 14:40:00 +0300

    fix: minor query changes

--- src/services/Cron.js
+++ src/services/Cron.js
@@ -2,7 +2,7 @@ const { runJob } = require('./Repositories');
 const cron = require('node-cron');
 
 const jobExecution = () => {
-    cron.schedule('0 0 * * * *', () => runJob());
+    cron.schedule('0 0 * * *', () => runJob());
 };
 
 module.exports = {
